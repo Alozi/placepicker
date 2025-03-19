1cb94c425c816d11e1bdd40e2a2c0ff9142de5fa
Alozi <alozimozi@gmail.com>
2025-03-19 16:54:27 +0200

    Add fetch function updateUserPlaces

--- src/http.js
+++ src/http.js
@@ -3,8 +3,28 @@ export async function fetchAvailablePlaces() {
     const resData = await response.json();
 
     if (!response.ok) {
-        throw new Error("Failed to fetch places");
+        throw new Error("Failed to fetch places.");
     }
 
     return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: 'PUT',
+        body: JSON.stringify({ places: places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error("Failed to update user data.");
+    }
+
+    console.log('resData');
+    console.log(resData);
+
+    return resData;
 }
